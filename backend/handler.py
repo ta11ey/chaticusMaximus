924dc59e37daf24b1574770226e296a5c6cf4592
jake <jake.ta113y@gmail.com>
2021-04-25 23:01:20 -0400

    insert working

--- backend/handler.py
+++ backend/handler.py
@@ -1,8 +1,13 @@
 import json
 import logging
+import boto3
+import logging
+import time
 
 logger = logging.getLogger("handler_logger")
 logger.setLevel(logging.DEBUG)
+
+dynamodb = boto3.resource("dynamodb")
 
 def ping(event, context):
     """
@@ -15,3 +20,24 @@ def ping(event, context):
 
     return response
 
+def insertTest(event, context):
+    """
+    test insert into db
+    """
+    logger.info("Ping requested")
+
+    #TESTING: make sure the DB works
+    table = dynamodb.Table("serverless-chat__Messages")
+    timestamp = int(time.time())
+
+    table.put_item(Item={"Room":"general", "Index": 0,
+        "Timestamp": timestamp, "Username": "ping-user",
+        "Content": "PING!"})
+    logger.debug("Item added to the database.")
+
+    response = {
+        "statusCode": 200,
+        "body": "PONG!"
+    }
+
+    return response
