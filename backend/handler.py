6ce46b2a074cfdc4e5d8535cfe4a44b6127b029b
jake <jake.ta113y@gmail.com>
2021-04-26 17:06:41 -0400

    feat: connections working

--- backend/handler.py
+++ backend/handler.py
@@ -7,7 +7,21 @@ import time
 logger = logging.getLogger("handler_logger")
 logger.setLevel(logging.DEBUG)
 
+CNXS_TABLE = "serverless-chat__Connections"
+MSG_TABLE = "serverless-chat__Messages"
 dynamodb = boto3.resource("dynamodb")
+
+def _get_body(event):
+    try:
+        return json.loads(event.get("body", ""))
+    except:
+        logger.debug("event body could not be JSON decoded.")
+        return {}
+
+def _get_response(status_code, body):
+    if not isinstance(body, str):
+        body = json.dumps(body)
+    return {"statusCode": status_code, "body": body}
 
 def ping(event, context):
     """
@@ -27,7 +41,7 @@ def insertTest(event, context):
     logger.info("Ping requested")
 
     #TESTING: make sure the DB works
-    table = dynamodb.Table("serverless-chat__Messages")
+    table = dynamodb.Table(MSG_TABLE)
     timestamp = int(time.time())
 
     table.put_item(Item={"Room":"general", "Index": 0,
@@ -41,3 +55,38 @@ def insertTest(event, context):
     }
 
     return response
+
+def connection_manager(event, context):
+    """
+    Handles connecting and disconnecting for the Websocket.
+    """
+    connectionID = event["requestContext"].get("connectionId", 123)
+
+    if event["requestContext"]["eventType"] == "CONNECT":
+        logger.info("Connect requested")
+        table = dynamodb.Table(CNXS_TABLE)
+        table.put_item(Item={"ConnectionID": connectionID})
+        return _get_response(200, "Connect Successful")
+    elif event["requestContext"]["eventType"] == "DISCONNECT":
+        logger.info("Disconnect requested")
+        table = dynamodb.Table(CNXS_TABLE)
+        table.delete_item(Key={"ConnectionID": connectionID})
+        return _get_response(200, "Disonnect Successful.")
+    else:
+        logger.info("Connection manager received unrecognized eventType.")
+        return _get_response(500, "Unrecognized eventType.")
+
+def send_message(event, context):
+    """
+    When a message is sent on the socket, forward it to all connections.
+    """
+    logger.info("Message sent on WebSocket.")
+
+    # Ensure all required fields were provided
+    body = _get_body(event)
+    for attribute in ["username", "content"]:
+        if attribute not in body:
+            logger.debug("Failed: '{}' not in message dict."\
+                    .format(attribute))
+            return _get_response(400, "'{}' not in message dict"\
+                    .format(attribute))
